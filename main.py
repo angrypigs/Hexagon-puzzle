d637b6b1f0fa07cb0e7fbb542f4ac3dd0826e1cb
angrypigs <angrypigs.tnt@gmail.com>
2023-06-21 22:34:54 +0200

    make function to create hexagon blocks

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ class App:
         # declare constants
         self.WIDTH = 700
         self.HEIGHT = 900
+        self.COLORS_PALETTE = [["", "#AC34F3", "#FBF8CC", "#90DBF4", "#FDE4CF",
+                                "#98F5E1", "#FFCFD2", "#B9FBC0", "#CFBAF0"]]
         # declare variables
         self.board = [[0 for i in range(j)] for j in [4, 5, 6, 7, 6, 5, 4]]
         self.current_cell = [-1, -1]
+        self.current_block = -1
+        self.blocks_to_choose = [-1, -1, -1]
         # init app
         self.master = tk.Tk()
         self.master.geometry(f"{self.WIDTH}x{self.HEIGHT}")
@@ -23,41 +27,63 @@ class App:
                                 bd=0, highlightthickness=0, bg="#123C61")
         self.canvas.place(x=0,y=0)
         self.canvas.create_rectangle(0, 0, self.WIDTH, self.HEIGHT,
-                                     fill="#0C2A43", tags="bg")
-        self.canvas.tag_bind("bg", "<Enter>", lambda event: self.background_event())
-        link = lambda x, y: (lambda event: self.cell_input(event, x, y))
+                                     fill="#0C2A43", tags=("bg"))
         for i in range(4):
             for j in range(4+i):
-                self.create_hexagon((7-i)*35+j*70, 200+60*i, "#174D7B", (), tags_bind=f"cell{i}_{j}")
-                self.canvas.tag_bind(f"cell{i}_{j}", "<Enter>", link(i, j))
+                self.create_hexagon((7-i)*35+j*70, 200+60*i, (f"cell{i}_{j}"), "#174D7B")
         for i in range(4, 7):
             for j in range(10-i):
-                self.create_hexagon((i+1)*35+j*70, 200+60*i, "#174D7B", (), tags_bind=f"cell{i}_{j}")
-                self.canvas.tag_bind(f"cell{i}_{j}", "<Enter>", link(i, j))
-                
-
+                self.create_hexagon((i+1)*35+j*70, 200+60*i, (f"cell{i}_{j}"), "#174D7B")
+        self.master.bind("<B1-Motion>", self.motion_left_clicked)
         self.master.mainloop()
 
-    def create_hexagon(self, x: float, y: float, color: str, 
-                       tags_list: tuple, text: str = "", tags_bind: str = "", size: float = 1) -> None:
+    def create_hexagon(self, x: float, y: float, global_tags: tuple,  
+                       color: str = "", text: str = "", size: float = 1, 
+                       coords_tag: str = "") -> None:
         """
         Function to create hexagon-like polygon
         """
-        tags_list2 = tags_list + (tags_bind, ) if tags_bind != "" else tags_list
+        tags_list2 = global_tags + (coords_tag, ) if coords_tag!="" else global_tags
         self.canvas.create_polygon([x, y-40*size, x+35*size, y-20*size, x+35*size, y+20*size,
                                     x, y+40*size, x-35*size, y+20*size, x-35*size, y-20*size],
-                                    fill=color, tags=tags_list2, width=2*size, outline="#000000")
+                                    fill=color, tags=global_tags, width=2*size, outline="#000000")
         if text != "": self.canvas.create_text(x, y, text=text, state='disabled',
                                                justify='center', anchor='center',
                                                font=font.Font(size=int(20*size), family="Helvetica"),
-                                               tags=tags_list)
+                                               tags=global_tags)
+            
+    def create_hexagon_block(self, x: int, y: int, mode: int, numbers: list|int, 
+                             global_tags: tuple, coords_tag: str = "") -> None:
+        """
+        Function to create single or double hexagon blocks ready to put in game
+        """
+        if mode==0:
+            self.create_hexagon(x, y, global_tags, self.COLORS_PALETTE[0][numbers], 
+                                text=str(numbers), coords_tag=coords_tag)
+        elif mode==1:
+            self.create_hexagon(x+35, y, global_tags, text=str(numbers[0]), 
+                                color=self.COLORS_PALETTE[0][numbers[0]], 
+                                coords_tag=coords_tag)
+            self.create_hexagon(x-35, y, global_tags, text=str(numbers[1]), 
+                                color=self.COLORS_PALETTE[0][numbers[1]])
+        elif mode==2:
+            self.create_hexagon(x, y, global_tags, text=str(numbers[0]), 
+                                color=self.COLORS_PALETTE[0][numbers[0]], 
+                                coords_tag=coords_tag)
+            self.create_hexagon(x-35, y-60, global_tags, text=str(numbers[1]), 
+                                color=self.COLORS_PALETTE[0][numbers[1]])
+        elif mode==3:
+            self.create_hexagon(x, y, global_tags, text=str(numbers[0]), 
+                                color=self.COLORS_PALETTE[0][numbers[0]], 
+                                coords_tag=coords_tag)
+            self.create_hexagon(x+35, y-60, global_tags, text=str(numbers[1]), 
+                                color=self.COLORS_PALETTE[0][numbers[1]])
+            
+            
 
-    def background_event(self) -> None:
-        print("lol")
-
-    def cell_input(self, event, a: int, b: int) -> None:
-        print(event)
-        print(a, b)
+    def motion_left_clicked(self, event) -> None:
+        self.canvas.itemcget(self.canvas.find_closest(event.x, event.y), "tags").split()[0]
+        print(self.canvas.coords(self.canvas.find_closest(event.x, event.y)))
 
 if __name__ == "__main__":
     app = App() 
