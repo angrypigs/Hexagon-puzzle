4eb0fd29745ad692df7e8ad10be1f18317b2b576
angrypigs <angrypigs.tnt@gmail.com>
2023-06-22 16:02:11 +0200

    add ability to create blocks and move them

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class App:
         self.current_cell = [-1, -1]
         self.current_block = -1
         self.blocks_to_choose = [-1, -1, -1]
+        self.unlocked_block = 2
         # init app
         self.master = tk.Tk()
         self.master.geometry(f"{self.WIDTH}x{self.HEIGHT}")
@@ -35,6 +36,8 @@ class App:
             for j in range(10-i):
                 self.create_hexagon((i+1)*35+j*70, 200+60*i, (f"cell{i}_{j}"), "#174D7B")
         self.master.bind("<B1-Motion>", self.motion_left_clicked)
+        self.master.bind("<ButtonRelease-1>", self.block_released)
+        self.generate_blocks()
         self.master.mainloop()
 
     def create_hexagon(self, x: float, y: float, global_tags: tuple,  
@@ -57,6 +60,7 @@ class App:
         """
         Function to create single or double hexagon blocks ready to put in game
         """
+        global_tags = tuple(global_tags.split(" "))
         if mode==0:
             self.create_hexagon(x, y, global_tags, self.COLORS_PALETTE[0][numbers], 
                                 text=str(numbers), coords_tag=coords_tag)
@@ -78,12 +82,42 @@ class App:
                                 coords_tag=coords_tag)
             self.create_hexagon(x+35, y-60, global_tags, text=str(numbers[1]), 
                                 color=self.COLORS_PALETTE[0][numbers[1]])
-            
-            
+
+    def generate_blocks(self, index: int = -1, block: list = []) -> None:
+        """
+        Generate three blocks under the gridcell or the specific one
+        """
+        if index == -1:
+            link = lambda x: (lambda event: self.block_icon_input(x))
+            for i in range(3):
+                self.blocks_to_choose[i] = [random.randint(0, 3)]
+                if self.blocks_to_choose[i][0] != 0:
+                    self.blocks_to_choose[i].extend(random.sample(range(1, self.unlocked_block+1), 2))
+                else:
+                    self.blocks_to_choose[i].append(random.choice(range(1, self.unlocked_block+1)))
+                l = self.blocks_to_choose[i][1] if self.blocks_to_choose[i][0] == 0 else self.blocks_to_choose[i][1:]
+                self.create_hexagon_block(self.WIDTH//4*(i+1), self.HEIGHT-100,
+                                          self.blocks_to_choose[i][0], l, 
+                                          (f"block{i}"), f"block{i}_coords")
+                self.canvas.tag_bind(f"block{i}", "<Button-1>", link(i))
+                
+    def block_icon_input(self, index: int) -> None:
+        self.current_block = index
 
     def motion_left_clicked(self, event) -> None:
-        self.canvas.itemcget(self.canvas.find_closest(event.x, event.y), "tags").split()[0]
-        print(self.canvas.coords(self.canvas.find_closest(event.x, event.y)))
+        if self.current_block != -1:
+            self.canvas.move(f"block{self.current_block}", 
+                             event.x-self.canvas.coords(f"block{self.current_block}")[0], 
+                             event.y-self.canvas.coords(f"block{self.current_block}")[1]-100)
+        tag = self.canvas.itemcget(self.canvas.find_closest(event.x, event.y), "tags").split()[0]
+        self.current_cell = [int(tag[4]), int(tag[6])] if "cell" in tag else [-1, -1]
+            
+
+    def block_released(self, event) -> None:
+        if self.current_block != -1:
+            print(self.current_cell)
+            self.canvas.move(f"block{self.current_block}", 
+                             self.WIDTH//4*(self.current_block+1)-event.x, self.HEIGHT-40-event.y)
 
 if __name__ == "__main__":
     app = App() 
